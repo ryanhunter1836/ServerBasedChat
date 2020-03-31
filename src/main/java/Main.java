6482416d185d7325648ca48a749b7f6a935dff70
Ryan Hunter <ryanhunter1836@gmail.com>
2020-03-30 19:09:36 -0500

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.io.PrintStream;
+
+public class Main {
+    public static void main(String[] args) {
+        print(System.out);
+    }
+
+    public static void print(PrintStream out) {
+        out.println("Hello, World!");
+    }
+}
