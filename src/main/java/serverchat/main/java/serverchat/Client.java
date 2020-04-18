8209d9a3a314a69ecb7a7b3b1dccfdb556b9142b
DanteMoreno <60411998+DanteMoreno@users.noreply.github.com>
2020-04-18 17:33:17 -0500

    Client.java to initiate ClientConnection
    Modified Client.java to input a socket to run ClientConnection for testing

--- src/main/java/serverchat/main/java/serverchat/Client.java
+++ src/main/java/serverchat/main/java/serverchat/Client.java
@@ -1,31 +1,18 @@
 package main.java.serverchat;
 
-/*
-
 import java.net.*;
 import java.io.*;
 
-public class Client
-{
-
-    public static void main(String argv[]) throws Exception
-    {
-        String textFromServer; //text from the server
-        Socket clientSocket = new Socket("localhost", 80); //host and port number subject to change
-
-        //Read Data from the Server
-        InputStream clientInput = clientSocket.getInputStream();
-        BufferedReader clientReader = new BufferedReader(new InputStreamReader(clientInput));
-        textFromServer= clientReader.readLine();
-        System.out.println(textFromServer); //print out data from server
-
-        //Send Data to server
-        OutputStream output = clientSocket.getOutputStream();
-        PrintWriter writer = new PrintWriter(output, true);
-        writer.println("This will be written to the server."); //write message to
-
-        clientSocket.close(); //terminate the connection
-    }
+public class Client {
+	public static void main(String args[]) throws Exception{
+  
+		//Initiate a client socket that ClientConnection can attempt to connect
+		Socket clientSocket = new Socket("localhost", 80); //host and port number subject to change
+		
+		//New object ClientConnection that takes clientSocket
+		ClientConnection client1 = new ClientConnection(clientSocket);
+		client1.run(); //runs input and output
+	
+  clientSocket.close(); //terminate the connection
+	}
 }
-
- */
