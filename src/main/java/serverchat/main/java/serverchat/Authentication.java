9375b46dc5da0fafb0698504bdd5691e32da49b9
Aaron Schuette <aaron.m.schuette@gmail.com>
2020-04-19 15:10:21 -0500

    Initial commit to Authentication branch
    Nothing special, just commiting new file to branch to start working on

---
+++ src/main/java/serverchat/main/java/serverchat/Authentication.java
@@ -0,0 +1,26 @@
+package main.java.serverchat;
+import java.util.*;
+
+public class Authentication
+{
+    private int rand;
+	
+	public Authentication()
+    {
+    	rand = Math.random();
+    	
+    }
+    
+    private void A3(rand, key) 
+    {
+    	// RES = hash1(rand + key)
+    }
+    
+    
+	
+	public static void main(String[] args)
+    {
+    	
+    }
+    
+}
