f1f5b79b7c47049184317253eb6c5826c2fcdb3a
Aaron Schuette <aaron.m.schuette@gmail.com>
2020-04-19 17:30:55 -0500

    Updating configuration errors with my eclipse

---
+++ src/main/java/serverchat/Authentication.java
@@ -0,0 +1,19 @@
+package main.java.serverchat;
+
+public class Authentication {
+	
+	private int rand;
+	private String key;
+	
+	public Authentication()
+	{
+		rand = (int) Math.random();
+	}
+	
+	private void A3()
+	{
+		// RES = hash1(rand + key)
+	}
+	
+	
+}
