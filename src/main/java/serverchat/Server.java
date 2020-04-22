98d745179a76c39c86127336348f8d2d0df4dd08
Aaron Schuette <aaron.m.schuette@gmail.com>
2020-04-22 01:00:33 -0500

    Reconfiguring branch and adding authentication

--- src/main/java/serverchat/Server.java
+++ src/main/java/serverchat/Server.java
@@ -1,28 +1,20 @@
 package main.java.serverchat;
 
-import java.io.IOException;
+import java.util.*;
 import java.net.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.io.*;
-import java.util.Hashtable;
 
 //Code is based off the documentation: https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ExecutorService.html
-//UDP Server sample code: https://www.baeldung.com/udp-in-java
 
-public class Server implements Message
+public class Server
 {
-    private String XRES;
-    private boolean listening = false;
-    //UDP socket used for authentication
-    private DatagramSocket serverSocket = null;
     int portNumber;
+    private boolean listening = false;
+    private ServerSocket serverSocket = null;
 
-    //Thread pool to hold all of the TCP client connections
     private final ExecutorService threadPool;
 
-    //UDP authentication server
     public Server(int port)
     {
         portNumber = port;
@@ -30,50 +22,82 @@ public class Server implements Message
         //Create the thread pool
         threadPool = Executors.newFixedThreadPool(10);
     }
-
+    
+    public void helloWait() throws Exception{
+    	DatagramSocket ds = new DatagramSocket(portNumber);
+    	
+    	byte[] receive = new byte[1000];
+    	DatagramPacket helloReceive = null;
+		DatagramPacket sendChallenge=null;
+		
+    	System.out.println("port number: "+ portNumber);
+		
+    	while(true) {
+    		helloReceive = new DatagramPacket(receive, receive.length);
+    		
+    		ds.receive(helloReceive);
+    		
+    		 String dataString =  new String(helloReceive.getData(), 0, helloReceive.getLength());
+    		 System.out.println(dataString);
+    			
+    		if(dataString.equals("hello")) {
+    			System.out.println("received hello");  
+    			
+    		   	//CHALLENGE
+    			int rand = (int)(Math.random()*100); //generates a random number to confirm    			
+    			receive = (rand+"").getBytes();
+    			String XRES = hash1(rand+"");
+    			sendChallenge = new DatagramPacket(receive, receive.length);
+    			sendChallenge.setAddress(helloReceive.getAddress());
+    			sendChallenge.setPort(helloReceive.getPort());
+    			System.out.println("Sending CHALLENGE");
+    			ds.send(sendChallenge);
+    			
+    			//Receiving RESPONSE
+    			helloReceive = new DatagramPacket(receive, receive.length);
+        		ds.receive(helloReceive);
+        		dataString =  new String(helloReceive.getData(), 0, helloReceive.getLength());
+        		String RES = hash1(dataString);
+        		
+        		if(RES.equals(XRES)) {
+    				System.out.println("AUTH_SUCCESS: Authorization process completed");
+    				receive = ("AUTH_SUCC").getBytes();
+    				sendChallenge = new DatagramPacket(receive, receive.length);
+        			sendChallenge.setAddress(helloReceive.getAddress());
+        			sendChallenge.setPort(helloReceive.getPort());
+        			
+        			ds.send(sendChallenge); //Send AUTH_SUCC to Client
+    				start(); //START TCP connection
+    				
+    			}else {
+    				System.out.println("AUTH_FAIL: Authorization process failed");
+    				break;
+    			}
+    		
+    		
+    		}
+    	}
+    }
+    
     //Call the start method to start the server
     public void start() throws Exception
     {
         listening = true;
         try {
-            serverSocket = new DatagramSocket(portNumber);
-            System.out.println("Authentication Server Listening");
+            serverSocket = new ServerSocket(portNumber);
+            System.out.println("Server Started");
 
             //Welcoming socket that accepts connections from a client then spins off into separate thread
             while(listening)
             {
-                byte[] buffer = new byte[Message.PacketLength];
-                DatagramPacket packet = new DatagramPacket(buffer, Message.PacketLength);
-                serverSocket.receive(packet);
-                System.out.println("Received connection from client");
-
-                //Decode the packet
-                //DecodedMessage message = (DecodedMessage)MessageFactory.decode(packet.getData());
-
-                //Get the message type, message, and destination routing details
-                //Message.MessageType messageType = message.messageType();
-                //String messageContent = message.message();
-
-                int clientPortNum = packet.getPort();
-                InetAddress clientAddress = packet.getAddress();
-
-                //Go through the authentication with the client
-                /*
-                if(inServer.readUTF().equalsIgnoreCase("hello")) {
-                	//CHALLENGE
-                	if(!challenge(inServer, outServer))
-                	{	
-                		outServer.writeUTF("AUTH_FAIL");
-                		break;
-                	}else {
-                		outServer.writeUTF("AUTH_SUCC");
-                	}
-                }
-                */
-               
-                //If client authentication is successful, spawn a new thread for the TCP connection
-                //NOTE: NEED TO REPLACE PORT NUMBER WITH A RANDOM PORT NUMBER
-                //threadPool.execute(new ClientConnection(500));
+                Socket socket = serverSocket.accept();
+                DataInputStream inServer = new DataInputStream(socket.getInputStream());
+                DataOutputStream outServer = new DataOutputStream(socket.getOutputStream());
+        
+              
+                //CONNECTED
+                System.out.println("CONNECTED: Received connection from client");
+                threadPool.execute(new ClientConnection(socket));
             }
         }
         catch(IOException e)
@@ -81,40 +105,16 @@ public class Server implements Message
             System.out.println(e.toString());
         }
     }
-
-    //Using Challenge
-    public boolean challenge(DataInputStream inServer, DataOutputStream outServer) throws Exception {
-    	int rand = (int)Math.random(); //generates a random number to confirm
-    	// output rand to client
-    	// get key from database
-    	
-    	XRES = hash1(rand + key); // get XRES to be compared to user RES
-    	
-    	// get client input of rand + key
-    	String RES = hash1(/*client input*/); // get RES
-    	
-    	outServer.writeUTF("Challenge key" + rand);
-    	
-    	if(inServer.readInt() == rand && XRES.equals(RES)) {
-    		outServer.writeUTF("CONNECTED TO SERVER"); //CONNECTED
-    		return true;
-    	}else {
-    		
-    		return false;
-    	}
-    
-    }
-    
+  
     // A3: Hashing function from key
     private String hash1(String key)
-    {	
-    	// super basic implementation, will probably change to account for collision
-    	MD5 m = new MD5();
-    	String res = m.getMD5(key);
-    	
-    	return res;
+    {
+        // super basic implementation, will probably change to account for collision
+        MD5 m = new MD5();
+        String res = m.getMD5(key);
+
+        return res;
     }
-    
     
     //Call the stop method to gracefully shutdown the server
     public void stop()
---
+++ src/main/java/serverchat/database/DatabaseExample.java
@@ -0,0 +1,114 @@
+package main.java.serverchat.database;
+
+import main.java.serverchat.database.Database;
+
+import org.bson.Document;
+
+/*
+ * Assumption made:
+ * - The clients will be created ahead of time with their private keys via DatabaseInitialize.java
+ */
+
+class DatabaseExample {
+    public static void main(String[] args) {
+        Database db = new Database();
+        Document clientA, clientB, clientC;
+        String sessionIDA, sessionIDB;
+
+        System.out.println("Create clients A, B, and C. Returns true on success, false on error.");
+        System.out.println(db.createClient("Client-ID-A", "somekey"));
+        System.out.println(db.createClient("Client-ID-B", "somekey"));
+        System.out.println(db.createClient("Client-ID-C", "somekey"));
+
+        System.out.println("\nAttempt to create client C again, but already exists.");
+        System.out.println(db.createClient("Client-ID-C", "somekey"));
+
+        System.out.println("\nGet Client A information.");
+        clientA = db.getClient("Client-ID-A");
+        System.out.println(clientA);                                    // All values of the client
+        System.out.println(clientA.getString("privateKey"));        // A specific value from the client, privateKey
+
+        System.out.println("\nAttempt to get Client D, which doesn't exist. Returns null.");
+        clientA = db.getClient("Client-ID-D");
+        System.out.println(clientA);
+
+        System.out.println("\nMake Client B connectable.");
+        System.out.println(db.makeClientConnectable("Client-ID-B"));
+        clientB = db.getClient("Client-ID-B");
+        System.out.println(clientB);
+
+        System.out.println("\nTry to make a session between A and B, but A is not connectable.");
+        System.out.println(db.connectClients("Client-ID-A", "Client-ID-B"));
+        clientA = db.getClient("Client-ID-A");
+        clientB = db.getClient("Client-ID-B");
+        System.out.println(clientA);
+        System.out.println(clientB);
+
+        System.out.println("\nMake Client A and Client C connectable.");
+        System.out.println(db.makeClientConnectable("Client-ID-A"));
+        System.out.println(db.makeClientConnectable("Client-ID-C"));
+        clientA = db.getClient("Client-ID-A");
+        clientC = db.getClient("Client-ID-C");
+        System.out.println(clientA);
+        System.out.println(clientC);
+
+        System.out.println("\nCreate a session between A and B for the first time.");
+        sessionIDA = db.connectClients("Client-ID-A", "Client-ID-B");
+        System.out.println(sessionIDA);
+        clientA = db.getClient("Client-ID-A");
+        clientB = db.getClient("Client-ID-B");
+        System.out.println(clientA);
+        System.out.println(clientB);
+
+        System.out.println("\nEmulate A and B sending messages to each other");
+        System.out.println(db.addChatHistory(sessionIDA, "Client-ID-A", "Message 1: Message from A"));
+        System.out.println(db.addChatHistory(sessionIDA, "Client-ID-A", "Message 2: Message from A"));
+        System.out.println(db.addChatHistory(sessionIDA, "Client-ID-B", "Message 3: Message from B"));
+        System.out.println(db.addChatHistory(sessionIDA, "Client-ID-A", "Message 4: Message from A"));
+        System.out.println(db.addChatHistory(sessionIDA, "Client-ID-B", "Message 5: Message from B"));
+
+        System.out.println("\nDisconnect the client A and B from the session.");
+        System.out.println(db.disconnectClients("Client-ID-A", "Client-ID-B"));
+        clientA = db.getClient("Client-ID-A");
+        clientB = db.getClient("Client-ID-B");
+        System.out.println(clientA);
+        System.out.println(clientB);
+
+        System.out.println("\nCreate a session between A and B again, but in reverse order. This shows order does " +
+                "not matter. The session is then ended.");
+        System.out.println(db.connectClients("Client-ID-B", "Client-ID-A"));
+        clientA = db.getClient("Client-ID-A");
+        clientB = db.getClient("Client-ID-B");
+        System.out.println(clientA);
+        System.out.println(clientB);
+        System.out.println(db.disconnectClients("Client-ID-A", "Client-ID-B"));
+
+        System.out.println("\nCreate a session between B and C.");
+        sessionIDB = db.connectClients("Client-ID-B", "Client-ID-C");
+        System.out.println(sessionIDB);
+        clientB = db.getClient("Client-ID-B");
+        clientC = db.getClient("Client-ID-C");
+        System.out.println(clientB);
+        System.out.println(clientC);
+
+        System.out.println("\nEmulate B and C sending messages to each other");
+        System.out.println(db.addChatHistory(sessionIDB, "Client-ID-B", "Message 1: Message from B"));
+        System.out.println(db.addChatHistory(sessionIDB, "Client-ID-C", "Message 2: Message from C"));
+        System.out.println(db.addChatHistory(sessionIDB, "Client-ID-B", "Message 3: Message from B"));
+        System.out.println(db.addChatHistory(sessionIDB, "Client-ID-C", "Message 4: Message from C"));
+        System.out.println(db.addChatHistory(sessionIDB, "Client-ID-B", "Message 5: Message from B"));
+
+        System.out.println("\nSession chat history between A and B:");
+        for (Document chatItem : db.getChatHistory(sessionIDA)) {
+            System.out.println(chatItem);
+        }
+
+        System.out.println("\nSession chat history between B and C:");
+        for (Document chatItem : db.getChatHistory(sessionIDB)) {
+            System.out.println(chatItem);
+        }
+
+        System.out.println("\nDelete the database.");
+//        db.dropDatabase();
+    }
+}
