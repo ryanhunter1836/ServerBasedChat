c153bfec0f656f2bd8eab4f00d2df7c4b3cbe721
DanteMoreno <60411998+DanteMoreno@users.noreply.github.com>
2020-04-23 03:25:17 -0500

    Modified to print output

--- src/main/java/serverchat/ClientConnection.java
+++ src/main/java/serverchat/ClientConnection.java
@@ -1,53 +1,55 @@
 package main.java.serverchat;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.*;
+import java.io.*;
+import java.net.*;
 
 //Instance of a connection between server and client
 public class ClientConnection implements Runnable, Message
 {
-    private int portNumber;
     private Socket socket;
-
-    public ClientConnection(int portNumber)
+    public String userName;
+    
+    public ClientConnection(Socket socket, String clientID)
     {
-        this.portNumber = portNumber;
+        this.socket = socket;
+        this.userName = clientID;
     }
 
+    public String getUserName() {
+    	return this.userName;
+    }
+
+    @Override
     //Required for the runnable interface
     public void run()
     {
         try
-        {
-            //Setup the listening socket
-            ServerSocket listeningSocket = new ServerSocket(portNumber);
-            //Accept the connection from the client
-            socket = listeningSocket.accept();
-
-            System.out.println("Received connection from client on port " + portNumber);
-            //Set up input and output byte streams
-            BufferedReader inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter outputStream = new PrintWriter(socket.getOutputStream(), true);
-
-            String inputLine = "";
+        {	
+            //Reading client input
+        	BufferedReader inputBuff = new BufferedReader(new InputStreamReader(System.in));
+        	        	
+        	//Reading from the server
+        	BufferedReader fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        	
+        	//writing to the server
+        	PrintWriter outWrite = new PrintWriter(socket.getOutputStream(), true);
+            
+        	String inputLine = "";
             String outputLine = "";
-
-            while ((inputLine = inputStream.readLine()) != null)
+                        
+            while (true)
             {
-                outputLine = inputLine + "\r";
-
-                //** EXAMPLE CODE
-                Message encoded = MessageFactory.encode(MessageType.CHAT, outputLine);
-                Message decoded = MessageFactory.decode(((EncodedMessage)encoded).encodedMessage());
-
-                //USE THIS METHOD TO GET THE BYTES TO SEND THROUGH THE SOCKET
-                // ((EncodedMessage)encoded).encodedMessage();
-
-                outputStream.println(outputLine);
+            	inputLine =inputBuff.readLine();
+            	outputLine = getUserName()+": "+ inputLine + "\r";
+                 
+            	outWrite.println(outputLine);
+            	outWrite.flush();
+            	
+            	//fromServer.readLine();
+            	
+            	System.out.println(fromServer.readLine());
+            	
                 if (inputLine.contains("Log off"))
                 {
                     break;
@@ -55,16 +57,35 @@ public class ClientConnection implements Runnable, Message
                 //Exit if the server is stopped and interrupts the thread
                 if(Thread.interrupted())
                 {
+                	System.out.println("Connection was interrupted!");
                     break;
                 }
             }
             System.out.println("Closing connection");
             socket.close();
-            inputStream.close();
+            
         }
         catch (IOException e)
         {
             System.out.println(e);
         }
     }
+    
+    public void chat(BufferedReader in, PrintWriter out, String recipient){
+    	boolean inChat = true;
+    	System.out.println("You are now in a chat with "+recipient);
+    	while(inChat) {
+    		
+    		 String outputLine =getUserName()+": "+ in + "\r";
+             
+
+             out.println(outputLine);
+    		
+    	}
+    	
+    	
+    }
+    
+    
+    
 }
